07513bd31a4efd1b9ec412b7e172b2437ff7f870
detlefarend <detlef@arend-privat.net>
2025-06-21 10:24:06 +0200

    Initial setup #2

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlpro_bm_suite',
+    version='0.1.0',
+    description='MLPro Benchmarking Suite – Scenarios, Benchmark Tests, and Results',
+    author='MLPro Team',
+    author_email='mlpro@listen.fh-swf.de',
+    license='Apache Software License 2.0',
+
+    packages=find_packages(where='benchmarking'),
+    package_dir={'': 'benchmarking'},
+
+    install_requires=[],
+
+    extras_require={
+        'full': [
+            'mlpro[full] @ git+https://github.com/fhswf/mlpro.git@main'
+        ]
+    },
+
+    zip_safe=False
+)
